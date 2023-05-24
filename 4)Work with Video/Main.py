e5adac4a9cfc4dd99a06726ad48a4318a1ec054e
Mazer1c <104981718+Mazer1c@users.noreply.github.com>
2023-05-24 21:37:28 +0300

    Update Main.py

--- 4)Work with Video/Main.py
+++ 4)Work with Video/Main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 vid = cv2.VideoCapture("Oshi no ko.mp4")
 
-def findingAngles(): #* Извлекаем из видео контуры.
+def findingAngles(): #* Извлекаем из видео контуры
     while True:
         success, frame = vid.read() #* Читаем входящий кадр
         grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #* Преобразуем кадр в серый цвет
